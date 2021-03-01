622af996e150aa6ab6ed79049f2352da2aa7f28c
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 04:08:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,7 +10,15 @@ class Author
     Post.all.select {|post| post.author == self}
   end
 
+  def add_post(post)
+    post.author = self
+  end
 
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+
+  end
 
 
 end
